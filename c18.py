3db8b986f64d280743b0aa01949315e31d3ac7e2
whythat <yuri.zhykin@gmail.com>
2017-02-26 13:55:22 +0200

    S03P18

---
+++ c18.py
@@ -0,0 +1,55 @@
+# ------------------------------------------------------------------------------
+# challenge 18
+#
+# Implement CTR, the stream cipher mode
+# ------------------------------------------------------------------------------
+import struct
+import base64
+from Crypto.Cipher import AES
+
+from c02 import bytes_xor
+
+
+# solution
+BSIZE = 16
+
+
+class KeyStreamEndException(Exception):
+    pass
+
+
+def aes_ctr_encrypt(ptext, key, nonce):
+    return _keystream_xor(ctext, key, nonce)
+
+
+def aes_ctr_decrypt(ctext, key, nonce):
+    return _keystream_xor(ctext, key, nonce)
+
+
+def _keystream_xor(data, key, nonce):
+    res = b''
+    for pad, chunk in zip(
+            _keystream(key, nonce),
+            [data[i:i + BSIZE] for i in range(0, len(data), BSIZE)]
+    ):
+        res += bytes_xor(chunk, pad)
+    return res
+
+
+def _keystream(key, nonce):
+    cipher = AES.new(key, AES.MODE_ECB)
+    nonce_bytes = struct.pack('<Q', nonce)
+    for count in range(2 ** 64 - 1):
+        count_bytes = struct.pack('<Q', count)
+        block = nonce_bytes + count_bytes
+        yield cipher.encrypt(block)
+    raise KeyStreamEndException()
+
+# testing
+if __name__ == "__main__":
+    string = base64.b64decode(
+        'L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/'
+        '2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ=='
+    )
+    key = b'YELLOW SUBMARINE'
+    print(aes_ctr_decrypt(string, key, 0))
